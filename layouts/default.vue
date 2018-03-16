0c46dc6bc4506d9a801db650ceb10b270060ad79
wittayasutt <wittayasutt@gmail.com>
2018-03-17 03:14:14 +0700

    initial

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+	<nuxt/>
+</template>
+
+<style lang="scss" scoped>
+	html {
+		font-family: 'Kanit';
+		box-sizing: border-box;
+	}
+
+	*,
+	*:before,
+	*:after {
+		box-sizing: border-box;
+		margin: 0;
+	}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<section class="main">
+		The Liberty To Act
+	</section>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				top: 0,
+				left: 0
+			}
+		},
+		components: {},
+		methods: {
+			escapeKeyListener: function(evt) {
+				if (evt.keyCode === 37) {
+					console.log('click left')
+				}
+				if (evt.keyCode === 38) {
+					console.log('click up')
+				}
+				if (evt.keyCode === 39) {
+					console.log('click right')
+				}
+				if (evt.keyCode === 40) {
+					console.log('click down')
+				}
+			}
+		},
+		mounted() {
+			document.addEventListener('keyup', this.escapeKeyListener)
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.main {
+		min-height: 100vh;
+		min-width: 100vw;
+		position: fixed;
+		background: blue;
+	}
+</style>
